070739835e792f6eb10bb69d8f3ae5d114b44191
humbertoluis22 <humbertoluis2013@gmail.com>
2025-04-18 22:57:16 -0300

    adicionando log

---
+++ ToDoList/Log/Logs.cs
@@ -0,0 +1,34 @@
+﻿namespace ToDoList.Log
+{
+    public static class Logs
+    {
+
+
+        public static void LogToFIle(string title, string texto)
+        {
+            var tituloArquivo = DateTime.Now.ToString("yyyyMMdd") + ".txt";
+
+            StreamWriter swLog;
+
+            if (File.Exists(tituloArquivo))
+            {
+                swLog = File.AppendText(tituloArquivo );
+            }
+            else
+            {
+                swLog = new StreamWriter(tituloArquivo);    
+            }
+            
+            swLog.WriteLine($"Log: ");
+            swLog.Write("Data : "  + 
+                DateTime.Now.ToLongDateString() + " " + 
+                DateTime.Now.ToLongTimeString());
+            swLog.WriteLine();
+            swLog.WriteLine("Titulo : " + title);   
+            swLog.WriteLine("Messagem : " + texto);
+            swLog.WriteLine("--------------------------------------------------");
+            swLog.WriteLine("");
+            swLog.Close();
+        }
+    }
+}
